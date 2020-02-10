0c7526c9a9619741684b3a22b97bfd3677f080e2
lishanshan <1549620779@qq.com>
2020-02-10 21:21:54 +0800

    日历组件导入成功

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <route-view></route-view>
+  </div>
     <router-view/>
   </div>
 </template>
